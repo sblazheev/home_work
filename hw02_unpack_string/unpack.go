4a950934c5f690b95f4f3723a55f903e829e16e7
Sergey Blazheev <s.blazheev@gmail.com>
2025-08-15 15:52:50 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,55 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	symbol := ""
+	slash := false
+	repeat := 0
+	builder := strings.Builder{}
+	for i, val := range str {
+		number, e := strconv.Atoi(string(val))
+		switch {
+		case e == nil && !slash:
+			if i == 0 {
+				return "", ErrInvalidString
+			}
+			if symbol == "" {
+				return "", ErrInvalidString
+			}
+			repeat = number
+			if repeat == 0 {
+				symbol = ""
+			}
+		case slash && e != nil && string(val) != `\`:
+			return "", ErrInvalidString
+		default:
+			if !slash && string(val) == `\` {
+				slash = true
+				continue
+			}
+			if symbol != "" {
+				builder.WriteString(symbol)
+				slash = false
+			}
+			symbol = string(val)
+		}
+
+		if repeat > 0 {
+			builder.WriteString(strings.Repeat(symbol, repeat))
+			symbol = ""
+			repeat = 0
+		}
+	}
+
+	if symbol != "" {
+		builder.WriteString(symbol)
+	}
+
+	return builder.String(), nil
 }
