288c300fb6c4255ea6b9c06388a17689df22ce56
Sergey Blazheev <s.blazheev@gmail.com>
2025-08-16 17:31:57 +0300

    first

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	bufferWordCount := map[string]int{}
+	text = strings.ReplaceAll(text, "\n", " ")
+	text = strings.ReplaceAll(text, "\t", "")
+	bufferWords := strings.Split(text, " ")
+	for _, str := range bufferWords {
+		if str == "" {
+			continue
+		}
+		bufferWordCount[str]++
+	}
+	keys := make([]string, 0, len(bufferWordCount))
+
+	for key := range bufferWordCount {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		if bufferWordCount[keys[i]] == bufferWordCount[keys[j]] {
+			return keys[i] < keys[j]
+		}
+		return bufferWordCount[keys[i]] > bufferWordCount[keys[j]]
+	})
+
+	if len(keys) > 10 {
+		keys = keys[0:10]
+	}
+
+	result := make([]string, 0, len(keys))
+
+	result = append(result, keys...)
+
+	return result
 }
