99eb74e64ed3dd463bf4e8a78b4a07f2e2bc7b5f
Sergey Blazheev <s.blazheev@gmail.com>
2025-09-17 02:23:15 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,35 @@ type (
 
 type Stage func(in In) (out Out)
 
+func inWrapper(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer func() {
+			close(out)
+			//nolint
+			for range in {
+			}
+		}()
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		in = inWrapper(in, done)
+		in = stage(in)
+	}
+
+	return in
 }
