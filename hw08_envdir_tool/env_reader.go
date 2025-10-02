370b8bcf21a1604aa1202dd9d08f99688c94ab15
Sergey Blazheev <s.blazheev@gmail.com>
2025-10-02 21:39:22 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,42 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	dirItems, err := os.ReadDir(dir)
+	envs := make(Environment, len(dirItems))
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range dirItems {
+		if !item.IsDir() {
+			content, errRead := readFirstLine(path.Join(dir, item.Name()))
+			if errRead != nil {
+				return nil, errRead
+			}
+			env := EnvValue{
+				Value:      strings.TrimRight(content, " \t"),
+				NeedRemove: false,
+			}
+			if len(content) == 0 {
+				env.NeedRemove = true
+			}
+			envName := strings.ReplaceAll(item.Name(), "=", "")
+			envs[envName] = env
+		}
+	}
+	return envs, nil
+}
+
+func readFirstLine(path string) (res string, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		buf := scanner.Text()
+		buf = strings.ReplaceAll(buf, "\x00", "\n")
+		return buf, nil
+	}
+	return
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	lEnvs, err := ReadDir(args[1])
+	if err != nil {
+		os.Exit(1)
+	}
+	envs := MergeOSEnv(os.Environ(), lEnvs)
+	exitCode := RunCmd(args[2:], envs)
+	if exitCode > 0 {
+		os.Exit(exitCode)
+	}
 }
